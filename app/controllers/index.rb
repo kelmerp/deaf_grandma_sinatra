3daa2b719212aa3a886d814ee925f0fefd9ffd1f
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 15:15:23 -0500

    Added logic for input

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,11 +4,23 @@ get '/' do
   erb :index
 end
 
+get '/foo' do
+  redirect 'http://localhost:9393/?grandma=foobar'
+end
+
 # http://localhost:9393/grandma
 # logic here
 # params: Params: {"user_input"=>"hello"}
 post '/grandma' do
   # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  
+
   @input = params[:user_input]
+  if @input == @input.upcase
+    @grandma = "NO, NOT SINCE 1983!"
+  elsif @input != @input.upcase
+    @grandma = "HUH?! SPEAK UP, SONNY!"
+  end
+
   erb :index
 end
